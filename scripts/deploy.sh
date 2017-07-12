8a1a544bfac5424c0d6f88dc5e746ef783ad2934
donnieSky <qmhs414@gmail.com>
2017-07-12 13:58:57 +0800

    update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+if [[ $TRAVIS_BRANCH == 'master' ]] ; then
   cd _site
   git init
 
@@ -13,3 +13,7 @@
   # We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
   git push --force --quiet "https://$REPO_TOKEN@github.com/donniesky/donniesky.github.io.git" master > /dev/null 2>&1
+else
+  echo 'Invalid branch. You can only deploy from master.'
+  exit 1
+fi
